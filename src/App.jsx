4a5bc12e9a084adef9902beb24825f1529da48a1
faysalahmed10 <faysalahmed.fa11@gmail.com>
2021-03-01 18:50:39 +0600

    increase and decrese work

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,18 @@
 import React,{useState} from 'react'
 
 const App = () =>{
-    const [number1,setnumber1] = useState(0)
-    const [number2,setnumber2] = useState(0)
-    const [result,setnumber] = useState(0)
-    const addNumber = ()=>{
-        const resltData = parseInt(number1) +parseInt(number2)
-        setnumber(resltData)
-    }
+   
+        const [number,setNumber] = useState(0)
+       
+  
     return (
         <>
             <h1>Hello world</h1>
-            <input type="number" onChange={(e)=>setnumber1(e.target.value)}  />
-            <input type="number" onChange={(e)=>setnumber2(e.target.value)}  />
-            <button onClick={addNumber} >Add</button>
-            <p>{number1}</p>
-            <p>{number2}</p>
-            <p>Reselt: {result}</p>
+           
+            <button onClick={()=> setNumber(number+1)} >+</button>
+            <p>{number}</p>
+            <button onClick={()=>setNumber(number-1)} >-</button>
+            
         </>
     )
 }
